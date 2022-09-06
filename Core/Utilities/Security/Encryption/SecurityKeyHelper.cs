095b132425d28a6faab267ecfd139e8ce87fc71a
canozdemir <canozdemir92@gmail.com>
2022-09-06 20:02:05 +0300

    delete spaces

--- Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
+++ Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -9,7 +9,6 @@ namespace Core.Utilities.Security.Encryption
 {
     public class SecurityKeyHelper
     {
-
         public static SecurityKey CreateSecurityKey(string securityKey)
         {
             return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
