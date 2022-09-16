580b7d16601e0a0922d7d6b3efdffa8df29c68f1
canozdemir <canozdemir92@gmail.com>
2022-09-16 17:46:14 +0300

    fix

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -33,7 +33,7 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        [SecuredOperation("product.add, admin")]
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
